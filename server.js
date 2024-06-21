575c585238b3f32fd884644a3894d8dedce0d90a
JacqueADev <jacquelineadev@gmail.com>
2024-06-21 00:56:54 -0300

    iniciando rotas e colocando status http

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+
+})
+
+
+
+app.get('/usuarios', (req, res) => {
+
+    res.status(200).json(users)
+
+})
+
+
+app.listen(3000)
+
+
+
+
+
+
+/* Objetivo:
+criar nossa api de usuarios.
+
+criar um
+listar todos
+editar um
+deletar um*/
